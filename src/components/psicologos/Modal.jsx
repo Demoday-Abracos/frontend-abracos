4109e385e70380d85ef0a5f03548c1b912212cf6
iarley <iarleysouza615@gmail.com>
2024-12-07 16:25:33 -0300

    update

--- src/components/psicologos/Modal.jsx
+++ src/components/psicologos/Modal.jsx
@@ -1,13 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Styles/Modal.css';
 
 function Modal({ onClose, psicologo }) {
+    const [formData, setFormData] = useState({
+        nome_consulta:'',
+        idade_consulta: '',
+        data_consulta: '',
+        horario_consulta: '',
+        motivo_consulta: '',
+        preferencias_consulta: ''
+    });
+
+    const [successMessage, setSuccessMessage] = useState(''); 
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('http://localhost:8080/consulta', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    ...formData,
+                    psicologoId: psicologo.id,
+                }),
+            });
+
+            if (response.ok) {
+                setSuccessMessage('Consulta agendada com sucesso!');
+                setFormData({
+                    nome_consulta: '',
+                    idade_consulta: '',
+                    data_consulta: '',
+                    horario_consulta: '',
+                    motivo_consulta: '',
+                    preferencias_consulta: ''
+                });
+            } else {
+                setSuccessMessage('Erro ao agendar consulta. Por favor, tente novamente.');
+            }
+        } catch (error) {
+            console.error('Erro:', error);
+            setSuccessMessage('Erro ao conectar com o servidor. Por favor, tente novamente.');
+        }
+    };
+
     return (
         <div className="modal-overlay">
             <div className="modal-content">
                 <button onClick={onClose} className="modal-close-button">Fechar</button>
                 <div className="modal-header">
-                    <img src={psicologo.foto} alt={"imagem do psicologo"} className="modal-psicologo-imagem" />
+                    <img src={psicologo.foto} alt="imagem do psicologo" className="modal-psicologo-imagem" />
                     <div className="header-text">
                         <h2>Agendamento de Sessão</h2>
                         <p><strong>Consulta - {psicologo.titulo}</strong></p>
@@ -16,36 +67,75 @@ function Modal({ onClose, psicologo }) {
                 </div>
 
                 <div className="modal-body">
-                    <form className="modal-form">
-                        <label>
-                            Nome:
-                            <input type="text" placeholder="Digite seu nome" />
-                        </label>
-                        <label>
-                            Idade:
-                            <input type="text" placeholder="Ex:18 anos..." />
-                        </label>
-                        <label>
-                            Data disponível:
-                            <input type="date" />
-                        </label>
-                        <label>
-                            Horário disponível:
-                            <input type="time" />
-                        </label>
-                        <label>
-                            Motivo da consulta:
-                            <input type="text" placeholder="Ex: Crises recentes..." />
-                        </label>
-                        <label>
-                            Preferência de consulta:
-                            <input type="text" placeholder="Ex: Atendimento em libras..." />
-                        </label>
+                    {successMessage ? (
+                        <div className="success-message">
+                            <h3>{successMessage}</h3>
+                            <button onClick={onClose} className="modal-button">
+                                Fechar
+                            </button>
+                        </div>
+                    ) : (
+                        <form className="modal-form" onSubmit={handleSubmit}>
+                            <label>
+                                Nome:
+                                <input type="text" name="nome_consulta" value={formData.nome_consulta} onChange={handleChange} placeholder="Digite seu nome" required />
+                            </label>
+                            <label>
+                                Idade:
+                                <input
+                                    type="number"
+                                    name="idade_consulta"
+                                    value={formData.idade_consulta}
+                                    onChange={handleChange}
+                                    placeholder="Ex:18 anos..."
+                                />
+                            </label>
+                            <label>
+                                Data disponível:
+                                <input
+                                    type="date"
+                                    name="data_consulta"
+                                    value={formData.data_consulta}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </label>
+                            <label>
+                                Horário disponível:
+                                <input
+                                    type="time"
+                                    name="horario_consulta"
+                                    value={formData.horario_consulta}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </label>
+                            <label>
+                                Motivo da consulta:
+                                <input
+                                    type="text"
+                                    name="motivo_consulta"
+                                    value={formData.motivo_consulta}
+                                    onChange={handleChange}
+                                    placeholder="Ex: Crises recentes..."
+                                    required
+                                />
+                            </label>
+                            <label>
+                                Preferência de consulta:
+                                <input
+                                    type="text"
+                                    name="preferencias_consulta"
+                                    value={formData.preferencias_consulta}
+                                    onChange={handleChange}
+                                    placeholder="Ex: Atendimento em libras..."
+                                />
+                            </label>
 
-                        <button type="submit" className="modal-button">Confirmar</button>
-                    </form>
+                            <button type="submit" className="modal-button">Confirmar</button>
+                        </form>
+                    )}
                 </div>
-
                 <div className="modal-footer">
                     <h3>Consulta - {psicologo.titulo}</h3>
                     <p>{psicologo.paragrafo}</p>
