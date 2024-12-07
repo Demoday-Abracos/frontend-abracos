b7c05e6318cf28c8a298ad92132aebe375f18ea1
iarley <iarleysouza615@gmail.com>
2024-12-07 00:43:39 -0300

    update

--- src/components/EditarPerfil/EditarPerfil.jsx
+++ src/components/EditarPerfil/EditarPerfil.jsx
@@ -22,33 +22,33 @@ function EditarPerfil() {
         <h2>Meu Perfil</h2>
         <p>Gerenciar e proteger sua conta</p>
         <form>
-          <label>
+          <label className="label-perfil">
             Nome de usuário
             <input type="text" placeholder="Digite seu nome de usuário" />
             <small>Nome do usuário pode ser alterado apenas uma vez.</small>
           </label>
-          <label>
+          <label className="label-perfil">
             Email
             <input type="email" placeholder="Digite seu email" />
           </label>
           <div className="gender-section">
             <span>Sexo</span>
-            <label>
+            <label className="label-perfil">
               <input type="radio" name="gender" value="masculino" /> Masculino
             </label>
-            <label>
+            <label className="label-perfil">
               <input type="radio" name="gender" value="feminino" /> Feminino
             </label>
-            <label>
+            <label className="label-perfil">
               <input type="radio" name="gender" value="outro" /> Outro
             </label>
           </div>
-          <label>
+          <label className="label-perfil">
             CPF/Data de nascimento
             <a href="#!" className="fill-now">
               Preencher agora
             </a>
-          </label>
+          </label >
           <button type="submit" className="save-button">
             Salvar
           </button>
