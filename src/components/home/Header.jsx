c970c089f9c8364c9c3f6425d5f78db9dac4a157
iarley <iarleysouza615@gmail.com>
2024-12-10 23:39:28 -0300

    uu

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import img from '../../../public/assets/images/Logo.png';
+import img from '../../../public/assets/images/logo.png';
 import '../../../src/components/Styles/Header.css';
 
 function Header() {
